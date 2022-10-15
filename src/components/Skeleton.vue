ac6d1c1c4a61ade42d2a41ef1e00464a02d82b19
Diego Pérez <dperez2055@gmail.com>
2022-10-15 14:04:46 -0300

    more components

---
+++ src/components/Skeleton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+    <div class="animate-pulse flex space-x-4">
+      <div class="rounded-full bg-slate-200 h-10 w-10" />
+      <div class="flex-1 space-y-6 py-1">
+        <div class="h-2 bg-slate-200 rounded" />
+        <div class="space-y-3">
+          <div class="grid grid-cols-3 gap-4">
+            <div class="h-2 bg-slate-200 rounded col-span-2" />
+            <div class="h-2 bg-slate-200 rounded col-span-1" />
+          </div>
+          <div class="h-2 bg-slate-200 rounded" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Skeleton',
+});
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <section class="m-4">
+      <Skeleton />
+    </section>
+    <section class="pt-2 mt-2">
+      <button
+        :class="`
+        flex-shrink-0
+        text-white
+        bg-${color}-500
+        border-0
+        py-2 px-8
+        focus:outline-none
+        hover:bg-${color}-600
+        rounded
+        text-lg
+        mt-10
+        sm:mt-0
+        transition ease-in-out delay-150 bg-${color}-500 hover:-translate-y-1 hover:scale-110 hover:bg-${color}-500 duration-150
+        `"
+      >
+        Button
+      </button>
+    </section>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Skeleton from '@/components/Skeleton.vue';
+
+export default defineComponent({
+  name: 'AboutView',
+  components: {
+    Skeleton
+  },
+  data: () => ({
+    color: 'red'
+  }),
+});
+</script>
